6000ad64b5ceee222d7f06061a5673240689941e
Anden Acitelli <a.acitelli@gmail.com>
2018-11-29 12:34:15 -0500

    Clarified Comments, Modified Indentation (Sorry, Lol)

--- main.cpp
+++ main.cpp
@@ -1,54 +1,69 @@
+// Including requisite libraries 
 #include <FEHLCD.h>
 #include <FEHIO.h>
 #include <FEHUtility.h>
 
+// Defining Constants 
+// These ones just cut down on code we have to write 
 #define BLACK FEHLCD::Black
 #define WHITE FEHLCD::White
+
+// Function Prototypes
 void drawMenu(FEHIcon::Icon *buttons, char labels[][20]);
+
 int main(void)
 {
     /*Clear screen, and set screen for writing*/
     LCD.Clear(BLACK);
     LCD.SetFontColor(WHITE);
 
-    /*Define Variables*/
+    // Defining Intro Screen Variables 
     int touchCount;
     float x,y;
-    bool run=true, press=true;
-    // declare an array of four icons called menu
+    bool run = true, press = true;
+
+    // Declaring Icon Array
     FEHIcon::Icon menu[4];
 
-    // define the four menu labels
+    // Defining Menu Text Labels
     char menu_labels[4][20] = {"START","RULES","CREDITS","STATS"};
 
-
-    while (run) {
+    // Draws the menu screen 
+    while (run)
+    {
         drawMenu(menu, menu_labels);
-        if(LCD.Touch(&x,&y)){
 
-            if(menu[0].Pressed(x,y,0)){
+        if(LCD.Touch(&x,&y))
+        {
+            if(menu[0].Pressed(x,y,0))
+            {
                 menu[1].WhilePressed(x,y);
                 LCD.Clear(BLACK);
                 LCD.WriteLine("PLAYING");
             }
-            if(menu[1].Pressed(x,y,0)){
+
+            if(menu[1].Pressed(x,y,0))
+            {
                 menu[1].WhilePressed(x,y);
                 LCD.Clear(BLACK);
                 LCD.WriteLine("RULES");
             }
-            if(menu[2].Pressed(x,y,0)){
+
+            if(menu[2].Pressed(x,y,0))
+            {
                 menu[2].WhilePressed(x,y);
                 LCD.Clear(BLACK);
                 LCD.WriteLine("CREDITS");
             }
-            if(menu[3].Pressed(x,y,0)){
+
+            if(menu[3].Pressed(x,y,0))
+            {
                 menu[3].WhilePressed(x,y);
                 LCD.Clear(BLACK);
                 LCD.WriteLine("STATS");
             }
         }
     }
-
 }
 
 void drawMenu(FEHIcon::Icon *buttons, char labels[][20])
